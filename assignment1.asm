4dbf087f2dbeb28c48cc4945a25fec3f6f4dd8ad
Whitney Griffith <whitneygriffith@Whitneys-MacBook-Pro.local>
2017-11-15 23:47:05 -0500

    Final2

--- assignment1.asm
+++ assignment1.asm
@@ -1,101 +1,150 @@
-#Variables for the program
 .data
+
+	userInput: .space 9
 	invalidMessage: .asciiz "Invalid hexadecimal number."
-	userInput: .space 9 #variable to hold string that user inputs
+	#userInput: .space 9 #variable to hold string that user inputs
 	lengthOfString: .word 0 #variable to store the length of the string
 	decimal: .word 0 #variable to store the final decimal
 	emptyOutput: .asciiz ""
 
-.text
 
+.text
 	#Function declaration to get text from user
 	main:
 	 li $v0,8 #take in input
          la $a0, userInput #load byte space into address
          li $a1, 9 # allot the byte space for string
-         move $t0,$a0 #save string to t0
-         syscall
-         
-         la $a0, userInput #reload byte space to primary address
-         move $a0,$t0 # primary address = t0 address (load pointer)
-	 #User input stored in $t0 and usersInput
-         
-   
+         syscall 
+    
 	#Gets the length of the string the user inputed
-	li $s1,0
-	la $s0, userInput
+	li $t1,0
+	la $t0, userInput
 	
 	loop:   
-		lb $a0,0($s0)
+		lb $a0,0($t0)
 		beqz $a0, out
-		addi $s0,$s0,1
-		addi $s1,$s1,1
+		addi $t0,$t0,1
+		addi $t1,$t1,1
 		j loop
 	out:    
 		#length has an extra 1 for the space
-		#subtract 1 from $s1
-		
-		sub $s1, $s1, 1
-		sw $s1, lengthOfString #Save length into lengthOfString 
+		sw $t1, lengthOfString #Save length into lengthOfString 
 
 
 		#Displays length of input
-		li $v0, 1
-		lw $a0, lengthOfString
-		syscall	
-	
-	
+		#li $v0, 1
+		#lw $a0, lengthOfString
+		#syscall	
+		
+       lw $s0, lengthOfString
+   
+       li $v0,1
+       la $a0, ($s0)   
 
-	
-	lw $s4, decimal #stores final answer
-	lw $s2, lengthOfString
-	sub $s2, $s2, 1 #counter that is being subtracted, ends when counter == 0, subtracts 1 because of indexing
-	#la $s0, userInput #holds the user string
-	li $s3, 0 #holds the chars
 
+       la $s1, userInput 
+       li $s3, 0 #holds the chars
+       
+       
+	lw $s2, decimal
+	addi $s5,$zero,16 
+	lw $s4, lengthOfString #counter that is being subtracted,
+	
 	#handle each character  
 	char:
-		lb $s3, 0($s0)
-		addi $s0, $s0, 1
-		
-		li $v0, 4
-		addi $a0, $s3, 0 
-		beq $s2, 1, sys
-		
-		
-		beq $s2, $zero, end
-		sub $s2, $s2, 1
-		#beqz $s2, emptyString #this is for the case of an empty string
-		
-		j char
-	
-		sys:
-		sw $s3, decimal
-		
+		lb $s3, 0($s1)
+		addi $s1, $s1, 1
+		beqz $s3,end
+    
+    
+    		beq $s3, 10, end
+   
+    		#subtract counter
+   		sub $s4, $s4, 1
+   
+    
+  		#checking for the character range
+
+    blt $s3, 48, error
+    blt $s3, 58, handleNum
+    blt $s3, 65, error
+    blt $s3, 71, uppercase
+    blt $s3, 97, error
+    blt $s3, 103, lowercase
+   
+    bgt $s3, 102, error 
+  	  	j char #continue loop
+
+	#This is the end of the loop. Outputs final decimal
+	end:
 		li $v0, 1
-		lw $a0, decimal
+		la $a0, ($s2) 
+		syscall 
+		li $v0, 10 #end program
 		syscall
-			
-	emptyString:
-	
-		bge $s4, 1, finalDec #this says that the string had itemts before so go to final decimal output
-		li $v0, 4
-		la $a0, emptyOutput
-		syscall
-	
-	finalDec:
-		li $v0, 1
-		lw $a0, decimal
-		syscall
-			
-	invalid:
-		li $v0, 4
+	#outputs invalid string
+	error:
+		li $v0, 4 
 		la $a0, invalidMessage
 		syscall
+		#end program here
+		li $v0, 10
+		syscall
 	
+	handleNum:
+		sub $t0, $s3, 48 
+		ori $t4,$zero,1 
+		ori $t5,$zero,0
+		beqz $s4, addDec
 		
+	addDec: 
+		mult $t0,$t4
+		mflo $t0
+		add $s2,$s2,$t0
+		j char
+
+	uppercase:
+		sub $t0, $s3, 55 
+		ori $t4,$zero,1 
+		ori $t5,$zero,0
+		beqz $s4, next
+	next: 
+		mult $t0,$t4
+		mflo $t0
+		add $s2,$s2,$t0
+		j char
 
 
-	 end:
-         li $v0,10 #end program
-         syscall
+	while:
+		mult $t4,$s5
+		mflo $t4
+		addi $t5,$t5,1
+		bne $t5,$s4,while
+
+
+	while2:
+		mult $t4,$s5
+		mflo $t4
+		addi $t5, $t5, 1
+		bne $t5, $s4, while2
+
+	lowercase:
+		sub $t0, $s3, 87 
+		ori $t4,$zero,1
+		ori $t5,$zero,0
+		beqz $s4, skip3
+	while3:
+		mult $t4, $s5
+		mflo $t4
+		addi $t5, $t5, 1
+		bne $t5, $s4, while3
+	skip3: 
+		mult $t0,$t4
+		mflo $t0
+		add $s2,$s2,$t0
+		j char
+
+	#end program
+	exit:
+		li $v0, 10
+		syscall
